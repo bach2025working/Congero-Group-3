1974d34a4ad3d8e03425e6a8aab11d707a1e659d
kevin_da_dino <58644950+dkhoapham@users.noreply.github.com>
2026-06-23 09:10:06 -0700

    Update fm_nai_act_rate.c

--- BRM/source/sys/fm_nai3_pol/fm_nai_act_rate.c
+++ BRM/source/sys/fm_nai3_pol/fm_nai_act_rate.c
@@ -436,11 +436,8 @@ fm_nai_act_rate(
         // 0 PIN_FLD_DESCR STR [0] "MYDESCR"
         PIN_FLIST_FLD_SET(cust_flist, PIN_FLD_DESCR, (void *)descr, ebufp);
 
-        // 0 PIN_FLD_INHERITED_INFO SUBSTRUCT [0]
-        pin_flist_t *inherited_info = PIN_FLIST_SUBSTR_ADD(cust_flist, PIN_FLD_INHERITED_INFO, ebufp);
-
         // 1     PIN_FLD_INFO_NAI3 SUBSTRUCT [0]
-        pin_flist_t *nextaig3 = PIN_FLIST_SUBSTR_ADD(inherited_info, PIN_FLD_INFO_NAI3, ebufp);
+        pin_flist_t *nextaig3 = PIN_FLIST_SUBSTR_ADD(cust_flist, PIN_FLD_INFO_NAI3, ebufp);
 
         // 2         PIN_FLD_MODELTYPE_NAI3 STR [0] "3.0 or 3.5"
         PIN_FLIST_FLD_SET(nextaig3, PIN_FLD_MODELTYPE_NAI3, (void *)model_code, ebufp);
